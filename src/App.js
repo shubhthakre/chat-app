4bfc5121e135a160dab40c0dc0e30a6ae6d3207e
shubh <thakres955@gmail.com>
2021-04-10 12:49:01 +0530

    creating chat rooms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine';
+
+import ChatFeed from './Components/ChatFeed';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>{
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="15fff267-0c2b-407a-bb32-6d644fbb7f65"
+            userName="shubh"
+            userSecret="123"
+            // renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+
+           
+   /> )
 }
 
 export default App;
---
+++ src/Components/ChatFeed.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const ChatFeed = (props) => {
+    console.log(props);
+
+
+    return(
+        <div>
+            ChatFeed
+        </div>
+    )
+}
+
+export default ChatFeed;
