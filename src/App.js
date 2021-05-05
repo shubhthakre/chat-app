ac51c91c90a0f5c9f9bb78a07de7635b5b414f8f
shubh <thakres955@gmail.com>
2021-05-05 23:52:34 +0530

    fix: signout

--- src/App.js
+++ src/App.js
@@ -3,22 +3,39 @@ import {ChatEngine} from 'react-chat-engine';
  import ChatFeed from './Components/ChatFeed';
 import './App.css';
 import LoginForm from './Components/LoginForm';
+import Darkmode from './Components/Darkmode';
 
 
 const App = () =>{
 
     if(!localStorage.getItem('username')) return <LoginForm/>
 
+    const handleSignOut = () => {
+        localStorage.setItem("username", "");
+        localStorage.setItem("password", "");
+        window.location.reload();
+      };
+    
+
     return(
+        <div>
+        <nav className="navbar" id="overlay">
+        
+         <button type="button" className="btn" onClick={handleSignOut}>
+          SignOut
+        </button>
+        </nav>
         <ChatEngine
-            height='100vh'
+        
+            height='97vh'
             userName={localStorage.getItem('username')}
             userSecret={localStorage.getItem('password')}
 			projectID='f66442c4-9c1a-4d93-a617-55b25a58df2d'
              renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 
            
-   /> )
+            />
+        </div> )
 }
 
 export default App;
